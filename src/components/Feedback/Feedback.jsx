f71618b47273bac96fcd47cc9d6915c322e9c2f1
sohel khan <sohelali9915@gmail.com>
2021-09-30 13:01:12 +0530

    Update

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,10 +1,32 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form, FormControl,Button } from "react-bootstrap";
+import "./Feedback.css";
 
 export const Feedback = () => {
   return (
+    <div>
     <Row>
-      <Col lg={8}>Amar</Col>
-      <Col lg={4}>Prem</Col>
+      <Col>
+      <Form>
+      <Form.Control placeholder="Enter the Title" className="fbrow"/ >
+      </Form>
+      </Col>
     </Row>
+    <Row>
+    <Form>
+    <FormControl as="textarea" placeholder="Enter Your Feedback" className="fbrow"  />
+    </Form>
+    </Row>
+    <Row>
+      <Col lg={10}></Col>
+      <Col lg={1}>
+        <Button>Sumbit</Button>
+      </Col>
+      <Col lg={1}>
+        <Button>Cancel</Button>
+      </Col>
+
+      
+    </Row>
+    </div>
   );
 };
